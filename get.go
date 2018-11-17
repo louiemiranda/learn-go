73c16450cc94be7c4c4f59aaba3ecca7a03479c2
Louie Miranda <louie@matchmove.com>
2018-11-17 11:43:11 +0800

    Added basic get, need to display the result still

--- get.go
+++ get.go
@@ -1,13 +1,39 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 const (
-	url = "http://www.mocky.io/v2/5bef87f92e00005244eeec30"
+	apiURL = "http://www.mocky.io/v2/5bef87f92e00005244eeec30"
 )
 
 func main() {
-	println(url)
+	println("Processing...")
 }
 
-func content() {
+func simpleGet() {
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		panic(err.Error)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error)
+	}
+
+	println(resp.StatusCode)
+	print(body)
+
+	// json.Unmarshal(body, &data)
+	// fmt.Printf("Result: %v\n", data)
+	// os.Exit(0)
+}
+
+func clientGet() {
 
 }
