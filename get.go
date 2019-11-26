ed387ccf12912bce108614fab41ba993d2e3a015
Louie Miranda <lmiranda@gmail.com>
2019-11-26 23:46:27 +0800

    learning updates

--- get.go
+++ get.go
@@ -1,74 +1,74 @@
-/**
- * jlxm: Learning Go-lang
- * Go-lang GET, HTTP/S, API, CLIENT/REQ/RES
- * References:
- * - https://golang.org/pkg/net/http/
- * - https://stackoverflow.com/questions/17156371/how-to-get-json-response-in-golang
- */
-package main
+// /**
+//  * jlxm: Learning Go-lang
+//  * Go-lang GET, HTTP/S, API, CLIENT/REQ/RES
+//  * References:
+//  * - https://golang.org/pkg/net/http/
+//  * - https://stackoverflow.com/questions/17156371/how-to-get-json-response-in-golang
+//  */
+// package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-)
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"os"
+// )
 
-const (
-	apiURL = "http://www.mocky.io/v2/5bef87f92e00005244eeec30"
-)
+// const (
+// 	apiURL = "http://www.mocky.io/v2/5bef87f92e00005244eeec30"
+// )
 
-func main() {
-	println("Processing...\n")
+// func main() {
+// 	println("Processing...\n")
 
-	simpleGet()
-	simpleGetJSON()
-}
+// 	simpleGet()
+// 	simpleGetJSON()
+// }
 
-// simpleGet: Retrieve raw body
-func simpleGet() {
+// // simpleGet: Retrieve raw body
+// func simpleGet() {
 
-	res, err := http.Get(apiURL)
-	if err != nil {
-		panic(err.Error)
-	}
+// 	res, err := http.Get(apiURL)
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
 
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+// 	body, err := ioutil.ReadAll(res.Body)
+// 	res.Body.Close()
 
-	if err != nil {
-		panic(err.Error)
-	}
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
 
-	// fmt.Println(runtime.Caller(0)) // find equivalent of __METHOD__ like on PHP?
-	fmt.Println("simpleGet():")
-	fmt.Println(res.StatusCode)
-	fmt.Printf("%s\n", body)
-}
+// 	// fmt.Println(runtime.Caller(0)) // find equivalent of __METHOD__ like on PHP?
+// 	fmt.Println("simpleGet():")
+// 	fmt.Println(res.StatusCode)
+// 	fmt.Printf("%s\n", body)
+// }
 
-func simpleGetJSON() {
-	res, err := http.Get(apiURL)
-	if err != nil {
-		panic(err.Error)
-	}
+// func simpleGetJSON() {
+// 	res, err := http.Get(apiURL)
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
 
-	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+// 	body, err := ioutil.ReadAll(res.Body)
+// 	res.Body.Close()
 
-	if err != nil {
-		panic(err.Error)
-	}
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
 
-	var data string
-	json.Unmarshal(body, &data)
+// 	var data string
+// 	json.Unmarshal(body, &data)
 
-	fmt.Println("\nsimpleGetJSON():")
-	fmt.Println(res.StatusCode)
-	fmt.Printf("%s\n", data)
-	os.Exit(0)
-}
+// 	fmt.Println("\nsimpleGetJSON():")
+// 	fmt.Println(res.StatusCode)
+// 	fmt.Printf("%s\n", data)
+// 	os.Exit(0)
+// }
 
-func clientGet() {
+// func clientGet() {
 
-}
+// }
--- getfunc.go
+++ getfunc.go
@@ -1,39 +1,39 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
-)
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"net/url"
+// 	"strings"
+// )
 
-func main() {
-	apiURL := "http://www.mocky.io"
-	resource := "/v2/5bef87f92e00005244eeec30"
-	data := url.Values{}
-	data.Add("x", "x")
-	data.Add("x", "x")
-	data.Add("x", "x")
-	data.Add("x", "x")
-	data.Add("x", "x")
-	data.Add("x", "10")
+// func mainx() {
+// 	apiURL := "http://www.mocky.io"
+// 	resource := "/v2/5bef87f92e00005244eeec30"
+// 	data := url.Values{}
+// 	data.Add("x", "x")
+// 	data.Add("x", "x")
+// 	data.Add("x", "x")
+// 	data.Add("x", "x")
+// 	data.Add("x", "x")
+// 	data.Add("x", "10")
 
-	u, _ := url.ParseRequestURI(apiURL)
-	u.Path = resource
-	urlStr := u.String() // 'https://api.com/user/'
+// 	u, _ := url.ParseRequestURI(apiURL)
+// 	u.Path = resource
+// 	urlStr := u.String() // 'https://api.com/user/'
 
-	// for control over http client headers
-	client := &http.Client{}
-	r, _ := http.NewRequest("POST", urlStr, strings.NewReader(data.Encode()))
-	r.Header.Add("X-Auth-Header", "x")
-	r.Header.Add("Content-Type", "application/json")
-	// r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+// 	// for control over http client headers
+// 	client := &http.Client{}
+// 	r, _ := http.NewRequest("POST", urlStr, strings.NewReader(data.Encode()))
+// 	r.Header.Add("X-Auth-Header", "x")
+// 	r.Header.Add("Content-Type", "application/json")
+// 	// r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
 
-	resp, _ := client.Do(r)
-	fmt.Println(resp.Status)
-	// fmt.Println(resp.ContentLength)
-	// fmt.Println(resp.Request)
-	// fmt.Println(json.NewDecoder(resp.Body.Decode()))
-	// json.Unmarshal(resp.Body, &data)
-	// fmt.Printf("Results: %v\n", data)
-}
+// 	resp, _ := client.Do(r)
+// 	fmt.Println(resp.Status)
+// 	// fmt.Println(resp.ContentLength)
+// 	// fmt.Println(resp.Request)
+// 	// fmt.Println(json.NewDecoder(resp.Body.Decode()))
+// 	// json.Unmarshal(resp.Body, &data)
+// 	// fmt.Printf("Results: %v\n", data)
+// }
