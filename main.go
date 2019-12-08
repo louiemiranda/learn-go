927e0a3e2c23fbc2b4e9ae75e8145ba6c2da3310
Louie Miranda <lmiranda@gmail.com>
2019-12-08 12:41:00 +0800

    x/time/rate working example

--- main.go
+++ main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
 )
+
+var limit = rate.Every(time.Second / 1)
+var limiter = NewIPRateLimiter(limit, 1)
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", okHandler)
+	mux.HandleFunc("/pass", okHandler)
 
-	// Wrap the servemux with the limit middleware.
-	log.Println("Listening on :8888...")
-	http.ListenAndServe(":8888", limit(mux))
+	if err := http.ListenAndServe(":8888", limitMiddleware(mux)); err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+}
+
+func limitMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log.Print("CHECK: r")
+		log.Print(r)
+		log.Print("URL")
+		log.Print(r.URL)
+
+		limiter := limiter.GetLimiter(r.RemoteAddr)
+
+		log.Print("CHECK: limiter")
+		log.Print(limiter)
+
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func okHandler(w http.ResponseWriter, r *http.Request) {
+	// Some very expensive database call
 	w.Write([]byte("OK"))
 }
